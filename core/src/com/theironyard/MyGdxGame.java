6002dbe852e8b5f5dbf52de273ae3ecc4c6b4fde
wrodgers95 <will.rodgers1995@gmail.com>
2016-08-16 19:51:34 -0400

    everything working

--- core/src/com/theironyard/MyGdxGame.java
+++ core/src/com/theironyard/MyGdxGame.java
@@ -6,18 +6,39 @@ import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 public class MyGdxGame extends ApplicationAdapter {
 
-	SpriteBatch batch;
-	Texture img;
 	float x, y, xv, yv;
-	static final float MAX_VELOCITY = 100;
+	static final float MAX_VELOCITY = 500;
+
+	SpriteBatch batch;
+	public TextureRegion left, right, up, down;
+
+	static final int WIDTH = 18;
+	static final int HEIGHT = 26;
+	static final int DRAW_WIDTH = WIDTH*3;
+	static final int DRAW_HEIGHT = HEIGHT*3;
+
+	boolean faceRight;
+	boolean faceUp;
+	boolean faceDown;
+	boolean faceLeft;
+
+
 
 	@Override
 	public void create() {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		Texture tiles = new Texture("tiles.png");
+		TextureRegion[][] grid = TextureRegion.split(tiles, 16, 16);
+		down = grid[6][0];
+		up = grid[6][1];
+		right = grid[6][3];
+		left = new TextureRegion(right);
+
+		left.flip(true, false);
 	}
 
 	@Override
@@ -25,15 +46,46 @@ public class MyGdxGame extends ApplicationAdapter {
 
 		move();
 
+		if (y > Gdx.graphics.getHeight()){
+			y = 0;
+		}
+		if (y < 0 ){
+			y = Gdx.graphics.getHeight();
+		}
+		if (x < 0 ){
+			x = Gdx.graphics.getWidth();
+		}
+		if (x > Gdx.graphics.getWidth()){
+			x = 0;
+		}
+
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, x, y);
+
+		if (faceUp) {
+
+			batch.draw(up, x, y, DRAW_WIDTH, DRAW_HEIGHT);
+
+		} else if (faceDown) {
+
+			batch.draw(down, x, y, DRAW_WIDTH, DRAW_HEIGHT);
+
+		} else if (faceRight) {
+
+			batch.draw(right, x, y, DRAW_WIDTH, DRAW_HEIGHT);
+
+		} else if (faceLeft) {
+
+			batch.draw(left, x, y, DRAW_WIDTH, DRAW_HEIGHT);
+		}
+
 		batch.end();
+
 	}
 
 	float decelerate(float velocity) {
-		float deceleration = 0.95f; // the closer to 1, the slower the deceleration
+		float deceleration = 0.95f;
 		velocity *= deceleration;
 		if (Math.abs(velocity) < 1) {
 			velocity = 0;
@@ -43,16 +95,47 @@ public class MyGdxGame extends ApplicationAdapter {
 
 	void move() {
 		if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
+			faceUp = true;
+			faceLeft = false;
+			faceDown = false;
+			faceRight = false;
 			yv = MAX_VELOCITY;
+			if (Gdx.input.isKeyPressed(Input.Keys.UP) && (Gdx.input.isKeyPressed(Input.Keys.SPACE))) {
+				yv = MAX_VELOCITY * 2;
+			}
 		}
 		if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
 			yv = MAX_VELOCITY * -1;
+			faceDown = true;
+			faceUp = false;
+			faceLeft = false;
+			faceRight = false;
+			if (Gdx.input.isKeyPressed(Input.Keys.DOWN) && (Gdx.input.isKeyPressed(Input.Keys.SPACE))) {
+				yv = MAX_VELOCITY *-2;
+			}
 		}
 		if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
 			xv = MAX_VELOCITY;
+			faceRight = true;
+			faceUp = false;
+			faceLeft = false;
+			faceDown = false;
+
+			if (Gdx.input.isKeyPressed(Input.Keys.RIGHT) && (Gdx.input.isKeyPressed(Input.Keys.SPACE))) {
+				xv = MAX_VELOCITY *2;
+
+			}
 		}
 		if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
-			xv = MAX_VELOCITY * -1;
+			xv = MAX_VELOCITY * -2;
+			faceLeft = true;
+			faceUp = false;
+			faceDown = false;
+			faceRight = false;
+
+			if (Gdx.input.isKeyPressed(Input.Keys.LEFT) && (Gdx.input.isKeyPressed(Input.Keys.SPACE))) {
+				xv = MAX_VELOCITY *-2;
+			}
 		}
 
 		y += yv * Gdx.graphics.getDeltaTime();
@@ -65,6 +148,5 @@ public class MyGdxGame extends ApplicationAdapter {
 	@Override
 	public void dispose() {
 		batch.dispose();
-		img.dispose();
 	}
 }
